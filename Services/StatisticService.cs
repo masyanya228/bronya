68a74d66f2048f8984d8d8b577f06711ab7644a3
marsel.khabibullin.99 <marsel.khabibullin.99@gmail.com>
2025-03-06 21:47:23 +0300

    [feat]: stats job with guest booking

--- Services/StatisticService.cs
+++ Services/StatisticService.cs
@@ -13,6 +13,7 @@ namespace Bronya.Services
             var yesterday = now.AddDays(-1);
             var smena = bookService.GetCurrentSmena(yesterday);
             var books = bookService.GetBooks(smena);
+            var booksByGuest = books.Where(x => x.Account.TGTag != "thegreenplacee").Count();
 
             AuthorizeService authorizeService = AuthorizeService.Instance;
             var admins = authorizeService.AccountService.AccountDS.GetAll()
@@ -20,11 +21,15 @@ namespace Bronya.Services
                 .Append(authorizeService.AccountService.AccountDS.Get(AccountService.MainTester.Id))
                 .ToList();
 
+            var textMsg = $"За вчерашную смену было {books.Count.TrueNumbers("бронь", "брони", "броней")}";
+            if (booksByGuest > 0)
+                textMsg += $"\r\nИз них {booksByGuest} от гостей";
+
             foreach (var admin in admins)
             {
                 authorizeService.TgAPI.SendOrEdit(
                     new DataPackage(admin),
-                    $"За вчерашную смену было {books.Count.TrueNumbers("бронь", "брони", "броней")}"
+                    textMsg
                 );
                 logService.LogEvent(nameof(SendStats) + ":" + admin?.Id);
             }
